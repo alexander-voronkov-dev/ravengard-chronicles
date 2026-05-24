2f9f08380325a90a1cda90d5f86df838dca786b2
Alexander Voronkov <voronkov_a_a@magnit.ru>
2026-05-24 23:16:01 +0300

    feat(story): add grey taiga story with details and roles

--- src/composables/useStories.ts
+++ src/composables/useStories.ts
@@ -50,9 +50,10 @@ export type StoryStatus = 'active' | 'completed' | 'archived'
 import { silverAlbatross } from '@/data/stories/1-silver-albatross'
 import { dinnerParty } from '@/data/stories/2-dinner-party'
 import { priceOfPerfection } from '@/data/stories/3-price-of-perfection'
+import { greyTaiga } from '@/data/stories/4-grey-taiga'
 
 export const useStories = (() => {
-  const stories: Story[] = [silverAlbatross, dinnerParty, priceOfPerfection]
+  const stories: Story[] = [silverAlbatross, dinnerParty, priceOfPerfection, greyTaiga]
 
   return () => {
     const getStories = () => stories
